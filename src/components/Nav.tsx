e40a83f63de5c51513bf683b36c87cf9d154a173
TITanZK <503838819@qq.com>
2021-03-12 11:34:20 +0800

    完成 Nav 页面的 icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -10,27 +13,44 @@ const NavWrapper = styled.nav`
     > li {
         width: 33.3333%;
         text-align: center;
-        padding: 16px;
+        padding: 2px 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .icon{
+          width: 28px;
+          height: 28px;
+        }
     }
   }
 `;
 
-const Nav = ()=>{
+const Nav = () => {
   return (
     <NavWrapper>
       <ul>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#money'/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#label'/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#statistics'/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
+  );
+};
 
 export default Nav;
