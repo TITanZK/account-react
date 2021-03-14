46c495ffc8cf341d658f77cf3854386146ae7f8c
TITanZK <503838819@qq.com>
2021-03-14 17:23:03 +0800

    Tags list 样式完成

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,12 +1,60 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    > .icon {
+      
+    }
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 10px 13px;
+  background: #767676;
+  border-radius: 4px;
+  color: white;
+`;
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Height = styled.div`
+  height: 16px;
+`
 
 function Tags() {
   const {tags, setTags} = useTags();
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <Icon name="right"/>
+          </li>)}
+      </TagList>
+      <Center>
+        <Height/>
+        <Height/>
+        <Height/>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
