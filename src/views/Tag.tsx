db2b9753b45a2e361eb32aa19bd3c686fbeee893
TITanZK <503838819@qq.com>
2021-03-16 14:56:07 +0800

    完成 Tag 回退功能

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
 import {useTags} from 'useTags';
 import Layout from 'components/Layout';
 import Icon from 'components/Icon';
@@ -20,6 +20,10 @@ const TopBar = styled.header`
   line-height: 20px;
   padding: 14px;
   background: white;
+  > button {
+    background: transparent;
+    border: none;
+  }
 `;
 const InputWrapper = styled.div`
   background: white;
@@ -34,33 +38,40 @@ const Tag: React.FC = () => {
   const {findTagId, updateTag, deleteTag} = useTags();
   const {tagId} = useParams<Params>();
   const tag = findTagId(parseInt(tagId));
+  let history = useHistory();
+
+  function handleClick() {
+    history.push("/tags");
+  }
   return (
     <Layout>
       <TopBar>
-        <Icon name="left"/>
+        <button onClick={handleClick}>
+          <Icon name="left"/>
+        </button>
         <span>编辑标签</span>
         <Icon/>
       </TopBar>
-      {tag ?  <div>
-                <InputWrapper>
-                  <Input label="标签名："
-                         placeholder="请输入修改信息"
-                         onChange={e => updateTag(tag.id, {name: e.target.value})}
-                         value={tag.name}/>
-                </InputWrapper>
-                <Center>
-                  <Height/>
-                  <Height/>
-                  <Height/>
-                  <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
-                </Center>
-              </div>
-            : <Center>
-                <Height/>
-                <Height/>
-                <Height/>
-                <div>标签不存在</div>
-              </Center>
+      {tag ? <div>
+          <InputWrapper>
+            <Input label="标签名："
+                   placeholder="请输入修改信息"
+                   onChange={e => updateTag(tag.id, {name: e.target.value})}
+                   value={tag.name}/>
+          </InputWrapper>
+          <Center>
+            <Height/>
+            <Height/>
+            <Height/>
+            <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
+          </Center>
+        </div>
+        : <Center>
+          <Height/>
+          <Height/>
+          <Height/>
+          <div>标签不存在</div>
+        </Center>
       }
     </Layout>
   );
