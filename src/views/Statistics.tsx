92818d342108f9903c5d41977b5c0d53f2dc9bdf
TITanZK <503838819@qq.com>
2021-03-17 20:22:06 +0800

    Statistics 页面样式及数据部分展示

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -9,6 +9,20 @@ import dayjs from 'dayjs';
 const CategoryWrapper = styled.section`
   background: white;
 `;
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+  > .note {
+    margin-right: auto;
+    margin-left: 16px;
+    font-size: 16px;
+    color: #999;
+  }
+`;
 
 function Statistics() {
   const [category, setCategory] = useState<'-' | '+'>('-');
@@ -20,16 +34,17 @@ function Statistics() {
       <CategoryWrapper>
         <CategorySection value={category} onChange={value => setCategory(value)}/>
       </CategoryWrapper>
-
-      <div>
-        {records.map(r => {
-          return <div>
-            {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
-            {r.amount}
-            {dayjs(r.createdAt).format('YYYY年MM月DD日')}
-          </div>;
-        })}
-      </div>
+      <div>{
+        records.map(r => {
+          return <Item>
+            <div className="tags">
+              {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+            </div>
+            {r.note && <div className="note">{r.note}</div>}
+            <div className="amount">￥{r.amount}</div>
+            {/*{r.amount}{dayjs(r.createdAt).format('YYYY年MM月DD日')}*/}
+          </Item>;
+        })}</div>
     </Layout>
   );
 }
