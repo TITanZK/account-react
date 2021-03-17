0da8d9884bfcc1c3d0bb5a950ab2bf2576416820
TITanZK <503838819@qq.com>
2021-03-17 19:07:26 +0800

    修复无法输入 . 的Bug

--- src/hooks/useUpdate.tsx
+++ src/hooks/useUpdate.tsx
@@ -4,7 +4,7 @@ export const useUpdate = (fn: () => void, deps: any[]) => {
   const count = useRef(0);
   useEffect(() => {
     count.current += 1;
-  },);
+  });
   useEffect(() => {
     if (count.current > 1) {
       fn();
